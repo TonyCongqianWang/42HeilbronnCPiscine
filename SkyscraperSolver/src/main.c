bd95ae289334e399276258f16821d579d5d3bb3e
Tony Wang <towang@student.42heilbronn.de>
2025-01-30 11:19:05 +0100

    add print utiliy

--- SkyscraperSolver/src/main.c
+++ SkyscraperSolver/src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: towang <towang@student.42.fr>              +#+  +:+       +#+        */
+/*   By: towang <towang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 11:51:38 by towang            #+#    #+#             */
-/*   Updated: 2025/01/28 18:20:13 by towang           ###   ########.fr       */
+/*   Updated: 2025/01/30 10:59:48 by towang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 int	main(int argc, char **argv)
 {
 	t_puzzle	puzzle;
+	int			cell_val;
 
 	if (argc != 2)
 	{
@@ -29,11 +30,19 @@ int	main(int argc, char **argv)
 		print_error("Wrong argument format.");
 		return (-2);
 	}
-	if (!solve_puzzle(&puzzle))
+	if (!tree_search(&puzzle, 1))
 	{
 		print_error("Could not find solution.");
 		return (0);
 	}
+	cell_val = 0;
+	while (cell_val < puzzle.size)
+	{
+		print_bmp_grid(&puzzle, cell_val, 1);
+		cell_val++;
+	}
+	print_score_grid(&puzzle);
+	print_message("");
 	print_solution_grid(&puzzle);
 	return (0);
 }
--- SkyscraperSolver/src/print_utlilty.c
+++ SkyscraperSolver/src/print_utlilty.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   print_utlilty.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: towang <towang@student.42.fr>              +#+  +:+       +#+        */
+/*   By: towang <towang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 11:55:36 by towang            #+#    #+#             */
-/*   Updated: 2025/01/28 17:49:01 by towang           ###   ########.fr       */
+/*   Updated: 2025/01/30 10:59:31 by towang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,72 @@ void	print_solution_grid(t_puzzle *puzzle)
 		}
 		counter++;
 	}
+}
+
+void	print_bmp_grid(t_puzzle *puzzle, int cell_val, int add_nl)
+{
+	int		counter;
+	char	print_val;
+	int		bitmap;
+
+	counter = 0;
+	while (counter < puzzle->size * puzzle->size)
+	{
+		bitmap = puzzle->node_state.valid_val_bmps[counter];
+		bitmap >>= (cell_val - 1);
+		bitmap &= 1;
+		print_val = '0' + bitmap;
+		write(1, &print_val, 1);
+		if (counter % puzzle->size != puzzle->size - 1)
+		{
+			write(1, " ", 1);
+		}
+		else
+		{
+			write(1, "\n", 1);
+		}
+		counter++;
+	}
+	if (add_nl)
+		write(1, "\n", 1);
+}
+
+void	print_score_grid(t_puzzle *puzzle)
+{
+	int		cell_idx;
+	int		bit_idx;
+	char	val;
+	int		bitmap;
+
+	cell_idx = 0;
+	while (cell_idx < puzzle->size * puzzle->size)
+	{
+		val = '0' + puzzle->size;
+		bitmap = puzzle->node_state.valid_val_bmps[cell_idx];
+		bit_idx = 0;
+		while (bit_idx < puzzle->size)
+		{
+			val -= bitmap & 1;
+			bitmap >>= 1;
+			bit_idx++;
+		}
+		write(1, &val, 1);
+		if (cell_idx % puzzle->size != puzzle->size - 1)
+			write(1, " ", 1);
+		else
+			write(1, "\n", 1);
+		cell_idx++;
+	}
+}
+
+void	print_message(const char *str)
+{
+	while (*str)
+	{
+		write(1, str, 1);
+		str++;
+	}
+	write(1, "\n", 1);
 }
 
 void	print_error(const char *str)
